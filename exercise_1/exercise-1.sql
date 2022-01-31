a7e5d3d7db846c1473edaf90021725284adc855b
EmilyDSarani <emily.sarani@gmail.com>
2022-01-31 13:36:48 -0600

    example 1 working

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,2 +1,10 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
+SELECT address.address_id
+FROM address
+INNER JOIN city
+ON address.city_id = city.city_id
+WHERE city.city = 'Dundee';
+
+--this was just to check to see if everything before WHERE was doing what I thought it was in correlation to the test
+-- WHERE address_id = 146;
