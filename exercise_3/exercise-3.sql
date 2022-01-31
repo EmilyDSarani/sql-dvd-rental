a843799836adae45dfd1c0a332fde41680994200
EmilyDSarani <emily.sarani@gmail.com>
2022-01-31 13:51:07 -0600

    three done

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,17 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+
+--select is like columns and where is like rows
+
+--okay so... first start with film...and we will want the title
+SELECT film.title
+FROM film
+--then, we want to left join into film_category
+LEFT JOIN film_category
+ON film.film_id = film_category.film_id
+--then we want to join into the category
+LEFT JOIN category
+ON film_category.category_id = category.category_id
+--then call the category we want
+WHERE category.name = 'Action'
